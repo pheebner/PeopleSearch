b77e47553e148b60500911ee03ff0cb081fc4a61
pheebner <pjheeb88@gmail.com>
2019-05-19 00:18:50 -0400

    Added more annotations

--- PeopleSearch.Persistence/Entities/Address.cs
+++ PeopleSearch.Persistence/Entities/Address.cs
@@ -1,18 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PeopleSearch.Persistence.Entities
 {
     public class Address
     {
+        [Key]
         public int Id { get; set; }
+        [ForeignKey(nameof(Person))]
+        [Required]
         public int PersonId { get; set; }
         [MaxLength(60)]
+        [Required]
         public string City { get; set; }
         [MaxLength(100)]
+        [Required]
         public string Street { get; set; }
         [MaxLength(9)]
+        [Required]
         public string ZipCode { get; set; }
         [MaxLength(55)]
+        [Required]
         public string Country { get; set; }
     }
 }
--- PeopleSearch.Persistence/Entities/Person.cs
+++ PeopleSearch.Persistence/Entities/Person.cs
@@ -5,13 +5,19 @@ namespace PeopleSearch.Persistence.Entities
 {
     public class Person
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         [MaxLength(50)]
         public string FirstName { get; set; }
+        [Required]
         [MaxLength(50)]
         public string LastName { get; set; }
+        [Required]
         public Address Address { get; set; }
+        [Required]
         public int Age { get; set; }
+        [Required]
         [Column(TypeName = "Text")]
         public string Interests { get; set; }
     }
