e79ba56f1cac8bfe52d114a018d6978f77f29651
pheebner <pjheeb88@gmail.com>
2019-05-20 20:06:54 -0400

    Rough draft of people search component

--- PeopleSearch/ClientApp/src/app/people-search/people-search.component.ts
+++ PeopleSearch/ClientApp/src/app/people-search/people-search.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fetch-data',
@@ -7,11 +9,36 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PeopleSearchComponent {
   public people: Person[];
+  public loading: boolean = false;
+  public searchboxTextChangeSubject: Subject<string> = new Subject<string>();
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Person[]>(baseUrl + 'api/Person/SearchByName?searchText=bann').subscribe(result => {
-      this.people = result;
-    }, error => console.error(error));
+  constructor(private httpClient: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+  }
+
+  ngOnInit(): void {
+    this.searchboxTextChangeSubject
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe(result => this.onSearchboxTextChanged(result));
+  }
+
+  onSearchboxTextChanged(searchText) {
+    if (searchText.length === 0) {
+      this.people = [];
+      return;
+    }
+
+    this.loading = true;
+
+    this.httpClient.get<Person[]>(this.baseUrl + 'api/Person/SearchByName?searchText=' + searchText)
+      .subscribe(
+        result => this.people = result,
+        null,
+        () => this.loading = false
+      );
+  }
+
+  onSearchBoxKeyupEvent($event) {
+    this.searchboxTextChangeSubject.next($event.target.value);
   }
 }
 
