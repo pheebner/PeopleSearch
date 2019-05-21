3b04a13c878cdfc670522109baa8f3ddc724c347
pheebner <pjheeb88@gmail.com>
2019-05-21 00:45:38 -0400

    Hooked up address to front end

--- PeopleSearch/ClientApp/src/app/people-search/person.model.ts
+++ PeopleSearch/ClientApp/src/app/people-search/person.model.ts
@@ -1,7 +1,10 @@
+import { Address } from "./address.model";
+
 export interface Person {
   firstName: string;
   lastName: string;
   age: number;
   interests: string;
   pictureUrl: string;
+  address: Address;
 }
