af124321507904dc9e35cfe6829398e7941a9484
pheebner <pjheeb88@gmail.com>
2019-05-21 22:27:03 -0400

    Added Web tests

---
+++ PeopleSearch.Tests/Controllers/PersonControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using PeopleSearch.Business.Interfaces;
+using PeopleSearch.Business.Models;
+using PeopleSearch.Controllers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleSearch.Tests.Controllers
+{
+    [TestFixture]
+    public class PersonControllerTests
+    {
+        private readonly IFixture _fixture = new Fixture();
+
+        [Test]
+        public async Task SearchByName_CallsServiceAndReturnsServiceResult()
+        {
+            var searchText = _fixture.Create<string>();
+            var expectedPeople = _fixture.CreateMany<Person>().ToList();
+            var personServiceMock = new Mock<IPersonService>();
+            personServiceMock.Setup(s => s.SearchByNameAsync(searchText)).ReturnsAsync(expectedPeople).Verifiable();
+            var sut = new PersonController(personServiceMock.Object);
+
+            var actualPeople = await sut.SearchByName(searchText);
+
+            personServiceMock.Verify();
+            actualPeople.Should().BeEquivalentTo(expectedPeople);
+        }
+    }
+}
